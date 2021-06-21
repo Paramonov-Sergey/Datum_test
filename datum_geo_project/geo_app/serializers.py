f5f88c6cc6a01c4cfcd8ab8aa6d30aad9465f767
sergey_paramonov <paramon23@list.ru>
2021-06-21 14:57:35 +0300

    added changes in serializers

--- datum_geo_project/geo_app/serializers.py
+++ datum_geo_project/geo_app/serializers.py
@@ -1,8 +1,45 @@
 from rest_framework import serializers
+from PIL import Image
 from rest_framework_gis.fields import GeometrySerializerMethodField
 
 from .models import City, Country, Capital
 from rest_framework_gis.serializers import GeoModelSerializer, GeoFeatureModelSerializer
+
+
+class Base64ImageField(serializers.ImageField):
+
+    def to_internal_value(self, data):
+        from django.core.files.base import ContentFile
+        import base64
+        import six
+        import uuid
+
+        if isinstance(data, six.string_types):
+            if 'data:' in data and ';base64,' in data:
+                header, data = data.split(';base64,')
+
+            try:
+                decoded_file = base64.b64decode(data)
+            except TypeError:
+                self.fail('invalid_image')
+
+            file_name = str(uuid.uuid4())[:12]
+
+            file_extension = self.get_file_extension(file_name, decoded_file)
+
+            complete_file_name = "%s.%s" % (file_name, file_extension,)
+
+            data = ContentFile(decoded_file, name=complete_file_name)
+
+        return super(Base64ImageField, self).to_internal_value(data)
+
+    def get_file_extension(self, file_name, decoded_file):
+        import imghdr
+
+        extension = imghdr.what(file_name, decoded_file)
+        extension = "jpg" if extension == "jpeg" else extension
+
+        return extension
 
 
 class CityListSerializer(GeoFeatureModelSerializer):
@@ -22,6 +59,10 @@ class CityCreateSerializer(GeoModelSerializer):
 
 
 class CityDetailSerializer(GeoFeatureModelSerializer):
+    image = Base64ImageField(
+        max_length=None, use_url=True,
+    )
+
     class Meta:
         geo_field = 'geometry'
         model = City
@@ -74,7 +115,6 @@ class CapitalSerializer(GeoModelSerializer):
 
     def update(self, instance, validated_data):
         city = City.objects.get(id=instance.city.id)
-        print(city, type(city))
         city.name = dict(validated_data['city']).get('name')
         city.geometry = dict(validated_data['city']).get('geometry')
         city.description = dict(validated_data['city']).get('description')
